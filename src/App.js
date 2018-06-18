a4f49c8461313774d5aab96d4f3b6da7e992d64b
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-18 14:28:02 -0400

    added styles to navlinks and the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import {Route, Switch, NavLink} from 'react-router-dom'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <h1>Api Party</h1>
+        <div className="App-header">
+          <h3>Ain't no party like an</h3>
+          <h1>Api Party</h1>
+        </div>
+
+         <ul className="navLinks">
+           <li>
+             <NavLink to="/github">GitHub</NavLink>
+           </li>
+         </ul>
+         <Switch>
+           <Route path = "/github" render={() => <h1>Github</h1>}/>
+           <Route render={() => <p>To get started, click one of the links above.</p>}/>
+         </Switch>
     
       </div>
     );
